88c51cdc2fd046ddc4d84d2596d3e99c678454e6
JugtarSingh <mail.jugtarsingh@gmail.com>
2025-11-11 01:08:38 +0530

    change UI

--- frontend/src/components/atoms/Template/Template.jsx
+++ frontend/src/components/atoms/Template/Template.jsx
@@ -1,29 +1,46 @@
 import Card from "../Card/Card";
-import imageUrl from '../../../assets/react.svg'
+import imageUrl from "../../../assets/react.svg";
 import Button from "../button/Button";
-import axios from '../../../config/axiosConfig.js';
-export default function Template() {
-    function handleOnClick(){
-        axios.post('/api/v1/projects');
-    }
-    return (
-        <>
-            <div className=" h-full bg-black w-full">
-                <div className="flex flex-col  justify-between ">
-                    <div className="mt-50 ml-50">
-                        <h1 className="text-6xl mb-4 bg-[linear-gradient(45deg,#FF8C00,#FF4500)] bg-clip-text text-transparent">Hello, Jugtar</h1>
-                        <p className="text-xl bg-[linear-gradient(45deg,#FF8C00,#FF4500)] bg-clip-text text-transparent">What do you want to build?</p>
-                    </div>
-                    <div class="relative z-10 bg-[#1e1e1e] rounded-2xl shadow-2xl w-3/4 h-[450px] mx-auto ">
-                        <div class="p-6 text-green-300 font-mono">
-                            <p>Start coding an app</p>
-                            <Button  onClick={handleOnClick} styleClass="m-4" text={<Card title="React" description="A JavaScript library for building user interfaces" imageUrl={imageUrl}/>}/>
-                            
-                        </div>
-                    </div>
+import axios from "../../../config/axiosConfig.js";
 
-                </div>
-            </div>
-        </>
-    )
+export default function Template() {
+  function handleOnClick() {
+    axios.post("/api/v1/projects");
+  }
+
+  return (
+    <div className="w-full min-h-screen bg-black flex flex-col items-center py-20">
+      {/* Header */}
+      <div className="text-center mb-16">
+        <h1 className="text-6xl font-bold mb-4 bg-gradient-to-r from-orange-400 to-orange-500 bg-clip-text text-transparent">
+          Hello, Jugtar
+        </h1>
+        <p className="text-xl bg-gradient-to-r from-orange-400 to-orange-500 bg-clip-text text-transparent">
+          What do you want to build?
+        </p>
+      </div>
+
+      {/* Main Card */}
+      <div className="relative z-10 w-3/4 max-w-4xl bg-[#1e1e1e]/80 backdrop-blur-xl rounded-3xl shadow-[0_0_40px_rgba(255,140,0,0.15)] p-10 border border-white/10">
+        <div className="text-green-300 font-mono mb-6">
+          <p className="text-lg">Start coding an app</p>
+        </div>
+
+        {/* Button + Card */}
+        <div className="flex justify-start">
+          <Button
+            onClick={handleOnClick}
+            styleClass="m-4 hover:scale-105 transition-all duration-200"
+            text={
+              <Card
+                title="React"
+                description="A JavaScript library for building user interfaces"
+                imageUrl={imageUrl}
+              />
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
